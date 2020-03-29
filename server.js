f86d3bed151c878f40e40c4f15929dcc71cce640
Matheus <matheusvbarone@gmail.com>
2020-03-29 17:52:18 +0200

    add cors

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require("express");
+const cors = require("cors");
 const mongoose = require("mongoose");
 const requireDir = require("require-dir");
 
 // Start App
 const app = express();
 app.use(express.json());
-
+app.use(cors());
 // Start DB
 mongoose.connect("mongodb://localhost:27017/nodeapi", {
   useUnifiedTopology: true,
